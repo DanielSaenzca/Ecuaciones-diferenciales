17009c9c90511db4a8490d2061bcb657532ef681
DanielSaenz <juan.saenzc@alumno.buap.mx>
2022-10-20 01:31:24 -0500

    Inciso 1a

--- Ecuaciones-diferenciales.py
+++ Ecuaciones-diferenciales.py
@@ -1,5 +1,5 @@
 
-#Inciso 1b
+#Inciso 1a
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
@@ -9,7 +9,7 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE" #Estos dos renglones son para arreglar el error OMP:Error #15
 from matplotlib import pyplot as plt
 import numpy as np 
-
+import math
 class ODEsolver(Sequential):
     def __init__(self, **kwargs): 
         super().__init__(**kwargs)
@@ -32,7 +32,7 @@ class ODEsolver(Sequential):
             x_o = tf.zeros((batch_size,1))
             y_o = self(x_o, training=True)
             #Esta es la ecuación 
-            eq = 1+ 2*x + 4*x*x*x
+            eq = y_prend-3*tf.sin(math.pi*x) #Esta parte me marca como error en VScode pero aun asi funciona el código
             ic = y_o 
             loss = keras.losses.mean_squared_error(0., eq) + keras.losses.mean_squared_error(0.,ic)
 
@@ -63,7 +63,7 @@ plt.figure(figsize = (10,10))
 
 plt.plot(x_testv, a)
 
-plt.plot(x_testv, 1+ 2*x + 4*x*x*x  , color = "green", linestyle='dashed')
+plt.plot(x_testv, 3*np.sin(math.pi*x)  , color = "green", linestyle='dashed')
 
 plt.show()
 exit()
