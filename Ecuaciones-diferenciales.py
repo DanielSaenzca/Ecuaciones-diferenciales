46a816bf7763869f23783696c6f73b925df345bc
DanielSaenz <juan.saenzc@alumno.buap.mx>
2022-10-20 04:23:11 -0500

    inciso 2b

--- Ecuaciones-diferenciales.py
+++ Ecuaciones-diferenciales.py
@@ -1,5 +1,5 @@
 
-#Inciso 2a
+#Inciso 2b
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
@@ -22,20 +22,26 @@ class ODEsolver(Sequential):
     def train_step(self, data):
         batch_size = tf.shape(data)[0]
         x = tf.random.uniform((batch_size, 1), minval = -2, maxval=2)
-
+        tf.GradientTape(persistent=True)
         with tf.GradientTape() as tape:
             # Compute the loss value
-            with tf.GradientTape() as tape2:
-                tape2.watch(x)
-                y_prend = self(x, training=True)
+            with tf.GradientTape() as tape1:
+                with tf.GradientTape() as tape2:
+                    tape2.watch(x)
+            y_prend = self(x, training=True)
             dy = tape2.gradient(y_prend, x)
+            tape1.watch(x)
+            d2y_dx2 = tape1.gradient(dy, x)
             x_o = tf.zeros((batch_size,1))
             y_o = self(x_o, training=True)
+            x_1 = tf.ones((batch_size, 1))
+            y_1 = self(x_1, training = True)
             #Esta es la ecuación 
-            eq = x* dy + y_prend - x*x*tf.cos(x) 
-            ic = y_o 
-            loss = keras.losses.mean_squared_error(0., eq) + keras.losses.mean_squared_error(0.,ic)
-
+            eq =  y_prend + d2y_dx2  
+            ic =  y_o  + 0.
+            ic1 = y_1 + 0.5
+            loss = keras.losses.mean_squared_error(0., eq) + keras.losses.mean_squared_error(0.,ic) + keras.losses.mean_squared_error(0.,ic1)
+            
         #Apply grads
         grads = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
@@ -54,7 +60,7 @@ model.summary()
 model.compile(optimizer=RMSprop(), metrics=['loss'])
 
 x=tf.linspace(-5,5,100)
-history = model.fit(x,epochs=2000,verbose= str(1))
+history = model.fit(x,epochs=2000,verbose=str(1))
 x_testv =tf.linspace(-5,5,100)
 a=model.predict(x_testv)
 #plt.plot(x_testv,a)
@@ -63,7 +69,7 @@ plt.figure(figsize = (10,10))
 
 plt.plot(x_testv, a)
 
-plt.plot(x_testv, x*np.sin(x) -2.*(-x*np.cos(x)+ np.sin(x))/x , color = "green", linestyle='dashed')
+plt.plot(x_testv, np.cos(x) -0.5*np.sin(x) , color = "green", linestyle='dashed')
 plt.show()
 exit()
 #Juan Daniel Saenz Carrizoza
